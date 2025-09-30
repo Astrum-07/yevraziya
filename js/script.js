63d233a23b47ac5e01bc3c79733f27f81960b8b9
Astrum-07 <astrumastro07@gmail.com>
2025-09-30 10:32:57 +0500

    asosiy page tugadi

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-  const menuBtn = document.getElementById('menu-btn');
-  const menu = document.getElementById('menu');
 
-  menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+  const btn = document.getElementById("menuBtn");
+  const icon = document.getElementById("menuIcon");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  btn.addEventListener("click", () => {
+    const isOpen = mobileMenu.classList.contains("max-h-[500px]");
+
+    if (!isOpen) {
+      // ochish
+      mobileMenu.classList.remove("max-h-0", "opacity-0");
+      mobileMenu.classList.add("max-h-[500px]", "opacity-100");
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-xmark");
+    } else {
+      // yopish
+      mobileMenu.classList.add("max-h-0", "opacity-0");
+      mobileMenu.classList.remove("max-h-[500px]", "opacity-100");
+      icon.classList.remove("fa-xmark");
+      icon.classList.add("fa-bars");
+    }
   });
